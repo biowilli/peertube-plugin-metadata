0bad43335a899cc1774f121e94ce9b36048a0026
philipp.monz <philipp.monz@fairkom.eu>
2023-09-20 13:54:19 +0200

    Fix 'Organizations' spelling. Hide for guests.

--- client/router-client-plugin.js
+++ client/router-client-plugin.js
@@ -1,36 +1,37 @@
-async function register({ registerHook }) {
+async function register({ registerHook, peertubeHelpers }) {
   registerHook({
     target: "filter:left-menu.links.create.result",
     handler: (links) => {
-      var newlinksection = {
-        key: "metadata",
-        title: "Metadata Plugin",
-        links: [
-          {
-            icon: "home",
-            path: "/p/metadata/creators",
-            shortLabel: "Creator",
-          },
-          {
-            icon: "home",
-            path: "/p/metadata/organizations",
-            shortLabel: "Organiztations",
-          },
-          {
-            icon: "home",
-            path: "/p/metadata/genres",
-            shortLabel: "Genre",
-          },
-          {
-            icon: "home",
-            path: "/admin/plugins/show/peertube-plugin-metadata",
-            shortLabel: "Settings",
-          },
-        ],
-      };
+      if (peertubeHelpers.isLoggedIn()) {
+        var newlinksection = {
+          key: "metadata",
+          title: "Metadata Plugin",
+          links: [
+            {
+              icon: "home",
+              path: "/p/metadata/creators",
+              shortLabel: "Creator",
+            },
+            {
+              icon: "home",
+              path: "/p/metadata/organizations",
+              shortLabel: "Organizations",
+            },
+            {
+              icon: "home",
+              path: "/p/metadata/genres",
+              shortLabel: "Genre",
+            },
+            {
+              icon: "home",
+              path: "/admin/plugins/show/peertube-plugin-metadata",
+              shortLabel: "Settings",
+            },
+          ],
+        };
 
-      links.push(newlinksection);
-
+        links.push(newlinksection);
+      }
       return links;
     },
   });
